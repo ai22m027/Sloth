9f6d62c95d994bfc696978349c7da7ddf609a0a4
Gerald Hirsch <gerald.hirsch@stappone.com>
2023-03-14 19:12:00 +0100

    done to window events, stopped at 18:06

--- Sloth/src/Sloth/Application.cpp
+++ Sloth/src/Sloth/Application.cpp
@@ -5,13 +5,20 @@
 
 namespace Sloth {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application() 
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application() {
 
+	}
+
+	void Application::OnEvent(Event& e) {
+		ST_CORE_INFO("{0}",e);
 	}
 
 	void Application::Run() {
